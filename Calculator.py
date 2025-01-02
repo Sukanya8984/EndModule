c5e62bea578660b3de72c02b1fd59fad0ddeb22f
Sukanya8984 <127278550+Sukanya8984@users.noreply.github.com>
2025-01-02 12:00:55 +0530

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,7 @@
+a= int(input("enter n1 :"))
+b= int(input("enter n2 :"))
+s=input("enter subtraction(-) or multiplication(*)")
+if s == '-' :
+    print("The result is =",a-b)
+elif s == '*':
+    print("The result is =",a*b)
